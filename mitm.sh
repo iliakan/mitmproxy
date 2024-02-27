54f94c5b513fe31eed205360e2c8d2840dbf70f5
Ilya Kantor <iliakan@users.noreply.github.com>
2024-02-27 10:45:35 +0100

    minor fixes

--- mitm.sh
+++ mitm.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
 
+# Go to browser
+# http://127.0.0.1:8081
+
+# Proxy at
+# http://127.0.0.1:8090
+
 source venv/bin/activate
 
 mitmweb --set allow_hosts='github|copilot' --set listen_port=8090 -s chat_completions_view.py -s copilot_codex_view.py -s jsonl_data_view.py
